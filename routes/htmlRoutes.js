216d3b3b5c9fa9e2bf61026bc11e007d5261b1d5
Will <williamjamesthompson@gmail.com>
2018-09-18 15:39:26 -0500

    Authentication is working now

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,54 +1,67 @@
 var db = require("../models");
 var path = require('path');
 
-module.exports = function(app) {
+module.exports = function (app, passport) {
   // Load index page
   // index route loads view.html
   app.get("/", function (req, res) {
-    res.render("login");
-  });
-
-  app.get("/survey", function (req, res) {
-    res.render("survey");
+    res.render("login", { layout: false });
   });
 
 
-  app.get("/rewards", function (req, res) {
-    res.render("rewards");
-  });
+  app.post('/',
+    passport.authenticate('local', { failureRedirect: '/' }),
+    function (req, res) {
+      res.redirect('/dash');
+    });
 
 
-  app.get("/dash", function (req, res) {
-    res.render("dashboard");
-  });
+  app.get('/survey',
+    require('connect-ensure-login').ensureLoggedIn(),
+    function (req, res) {
+      res.render('survey', { user: req.user });
+    });
 
-  // cms route loads cms.html
-  app.get("/cms", function (req, res) {
-    res.render("cms");
-  });
+
+  app.get('/rewards',
+    require('connect-ensure-login').ensureLoggedIn(),
+    function (req, res) {
+      res.render('rewards');
+    });
+
+
+
+  app.get('/dash',
+    require('connect-ensure-login').ensureLoggedIn(),
+    function (req, res) {
+      res.render('dashboard', { user: req.user });
+    });
+
+
+
+  app.get('/cms',
+    require('connect-ensure-login').ensureLoggedIn(),
+    function (req, res) {
+      res.render('cms', { user: req.user });
+    });
 
   // blog route loads blog.html
   app.get("/categories", function (req, res) {
     res.render("category-manager");
   });
 
+
   app.get("/employees", function (req, res) {
     res.render("employee-manager");
   });
 
-  // app.get("/", function (req, res) {
-  //   db.Example.findAll({}).then(function (dbExamples) {
-  //     res.render("index", {
-  //       msg: "Welcome!",
-  //       examples: dbExamples
-  //     });
-  //   });
-  // });
-  
-  app.get("/login", function (req, res) {
-    res.render("login");
-  });
-  
+  app.get('/logout',
+    function (req, res) {
+      req.logout();
+      res.redirect('/');
+    });
+
+
   // Load example page and pass in an example by id
   app.get("/example/:id", function (req, res) {
     db.Example.findOne({ where: { id: req.params.id } }).then(function (dbExample) {
@@ -57,11 +70,11 @@ module.exports = function(app) {
       });
     });
   });
-  
+
   // Render 404 page for any unmatched routes
   app.get("*", function (req, res) {
-    res.render("404");
+    res.render("404", { layout: false });
   });
-  
+
 
 };
