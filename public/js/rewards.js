b687c69ff2d48c63706a9cd83534e5fa01495539
mmcadet@gmail.com <34467088+mmcadet@users.noreply.github.com>
2018-09-22 10:07:28 -0500

    more changes

--- public/js/rewards.js
+++ public/js/rewards.js
@@ -1,43 +1,3 @@
-
-// $(document).ready(function() {
-// $.ajax({
-//   url: "https://api.discountapi.com/v2/deals?api_key=mAzPLCrk",
-//   type: "GET",
-//   data: {
-//     $limit: 1000
-//   }
-// }).done(function(data) {
-//   alert("Retrieved " + data.deals.length + " deals from the dataset!");
-//   console.log(data.deals[0]);
-//   for (i = 0; i < data.deals.length; i++) {
-//     $("#deal-view").append("<h1> Title: " + data.deals.deal.title[i] + "</h1>");
-//   }
-//   $("#deal-view").text(JSON.stringify(data));
-// });
-// });
-
-// var template = $("#itemTemplate").text();
-
-// // Handlebars compiles the template into a callable function
-// var renderer = Handlebars.compile(template);
-// call the compiled function with the template data
-// for (i=0; i<data.deals.length; i++) {
-//     var result = renderer({
-//         "deal" : data.deals.deal.title[i],
-//         "img" : data.deals.deal.image_url[i],
-//         "description" : data.deals.deal.description[i],
-//         "data" : {
-//             "price" : data.deals.deal.price[i],
-//             "inStock" : data.deals.deal.online[i]
-//         }
-//     });
-// console.log(result);
-// }
-
-// $("#container").html(result);
-// });
-
-
 var queryURL = "https://api.discountapi.com/v2/deals?api_key=mAzPLCrk";
 $.ajax({
   url: queryURL,
@@ -47,7 +7,7 @@ $.ajax({
   for (let i = 0; i < response.deals.length; i++) {
     // Print each iteration to the console
     console.log(response.deals[i].deal.title);
-    $('#deal-view').append(response.deals[i].deal.title + "<br>");
+    $('#deal-view').append(response.deals[i].deal.title + "<br><br>");
   }
 });
 
