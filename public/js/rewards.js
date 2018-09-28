5f8f0b50b882e80f2ebd3d9b4f83c4647c6db291
mmcadet@gmail.com <34467088+mmcadet@users.noreply.github.com>
2018-09-27 20:24:18 -0500

    added more rewards

--- public/js/rewards.js
+++ public/js/rewards.js
@@ -5,9 +5,41 @@ $.ajax({
 }).then(function (response) {
   console.log(response.deals);
   for (let i = 0; i < response.deals.length; i++) {
+     
     // Print each iteration to the console
+    var title = $("<h2>");
+    title.text(response.deals[i].deal.title);
+
+    var img = $("<img>");
+    img.attr("src",response.deals[i].deal.image_url);
+    img.addClass("deal-img");
+
+    var price = $("<h4>$");
+    price.text(response.deals[i].deal.price);
+
+    var url = $("<a>");
+    url.attr("href",response.deals[i].deal.url);
+    url.text("Grab this deal!");
+
+
     console.log(response.deals[i].deal.title);
-    $('#deal-view').append(response.deals[i].deal.title + "<br><br>");
+    console.log(response.deals[i].deal.url);
+
+    // Print the title of each deal
+    $('#deal-view').append(title);
+    $('#deal-view').append("<br/>");
+
+    // Show the image of each deal
+    $('#deal-view').append(img);
+    $('#deal-view').append("<br/>");
+
+    // Print the title of each deal
+    $('#deal-view').append(price);
+    $('#deal-view').append("<br/>");
+
+    // Print url of each deal
+    $('#deal-view').append(url);
+    $('#deal-view').append("<br/>");
   }
 });
 
